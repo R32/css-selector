54293a4f8781b8fbe2d7b1a8e903ce9a70f83a79
r32 <56782055@qq.com>
2018-12-28 16:47:49 +0800

    minor

--- release.sh
+++ release.sh
@@ -5,5 +5,5 @@ mkdir -p release
 cp -R -u csss haxelib.json README.md release
 chmod -R 777 release
 cd release
-zip -r release.zip ./ -x 'csss/Utils.hx' && mv release.zip ../
+zip -r release.zip ./ && mv release.zip ../
 cd ..
