3343c2a6ca6775d5cb4cfedfb50ecce7c332c9d0
r32 <56782055@qq.com>
2018-12-28 20:21:32 +0800

    ++

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 #
 rm -rf release
 mkdir -p release
-cp -R -u csss haxelib.json README.md release
+cp -R -u csss haxelib.json release
 chmod -R 777 release
 cd release
 zip -r release.zip ./ && mv release.zip ../
